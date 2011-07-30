90957e4ee6dca8164ec49d5d037f9d4916b92103
Christopher Schuster <cs@livoris.de>
2011-07-30 19:20:41 +0200

    refactored context and layer

--- src/de/livoris/dietcop/Context.java
+++ src/de/livoris/dietcop/Context.java
@@ -3,13 +3,11 @@ package de.livoris.dietcop;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
-import de.livoris.dietcop.Layer.AppliesTo;
 
 public final class Context {
 	
@@ -52,44 +50,16 @@ public final class Context {
 			});
 	}
 
-	private static Callable<Object> layer(final Layer layer,
-			final Callable<?> next, final Method m, final Object obj,
-			final Object[] args) {
-		return new Callable<Object>() {
-			@Override
-			public Object call() throws Exception {
-				ArrayList<Object> newArgs = new ArrayList<Object>(Arrays.asList(args == null ? new Object[0] : args));
-				newArgs.add(0, obj);
-				newArgs.add(0, next);
-				return m.invoke(layer, newArgs.toArray());
-			}
-		};
-	}
-
 	private static Object executeLayered(final Object obj, final Method method, final Object[] args) throws Exception {
-		Callable<Object> current = new Callable<Object>() {
+		Callable<Object> top = new Callable<Object>() {		
 			@Override
 			public Object call() throws Exception {
 				return method.invoke(obj, args);
 			}
 		};
-		List<Class<?>> methodParameterTypes = Arrays.asList(method.getParameterTypes());
-		for (Layer l : activeLayers.get()) {
-			for (Method m : l.getClass().getDeclaredMethods()) {
-				AppliesTo at = m.getAnnotation(AppliesTo.class);
-				for (Class<?> intf : obj.getClass().getInterfaces()) {
-					if (at != null && at.value().equals(intf)
-						&& method.getName().equals(m.getName())) {
-						List<Class<?>> mParameterTypes = new ArrayList<Class<?>>(Arrays.asList(m.getParameterTypes()));
-						if (Callable.class.equals(mParameterTypes.remove(0))
-							&& at.value().equals(mParameterTypes.remove(0))
-							&& methodParameterTypes.equals(mParameterTypes)) {
-							current = layer(l, current, m, obj, args);
-						}
-					}
-				}
-			}
+		for (Layer layer : activeLayers.get()) {
+			top = layer.chain(obj, method, args, top);
 		}
-		return current.call();
+		return top.call();
 	}
 }
--- src/de/livoris/dietcop/Layer.java
+++ src/de/livoris/dietcop/Layer.java
@@ -4,11 +4,44 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.Callable;
 
 public abstract class Layer {
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.METHOD)
 	public static @interface AppliesTo {
 		Class<?> value();
+	}
+	
+	private Callable<Object> layer(final Callable<?> next, final Object obj, final Method layerMethod, final Object[] args) {
+		return new Callable<Object>() {
+			@Override
+			public Object call() throws Exception {
+				ArrayList<Object> newArgs = new ArrayList<Object>(Arrays.asList(args == null ? new Object[0] : args));
+				newArgs.add(0, obj);
+				newArgs.add(0, next);
+				return layerMethod.invoke(Layer.this, newArgs.toArray());
+			}
+		};
+	}
+
+	Callable<Object> chain(final Object obj, final Method method, final Object[] args, Callable<Object> next) {
+		for (Method layerMethod : getClass().getDeclaredMethods()) {
+			AppliesTo at = layerMethod.getAnnotation(AppliesTo.class);
+			for (Class<?> intf : obj.getClass().getInterfaces()) {
+				if (at != null && at.value().equals(intf) && method.getName().equals(layerMethod.getName())) {
+					ArrayList<Class<?>> mParameterTypes = new ArrayList<Class<?>>(Arrays.asList(layerMethod.getParameterTypes()));
+					if (Callable.class.equals(mParameterTypes.remove(0))
+						&& at.value().equals(mParameterTypes.remove(0))
+						&& mParameterTypes.equals(Arrays.asList(method.getParameterTypes()))) {
+						return layer(next, obj, layerMethod, args);
+					}
+				}
+			}
+		}
+		return next;
 	}
 }
